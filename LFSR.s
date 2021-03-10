e4a6e36bdec1b5bfc79e0707637c3ba0c8dea628
Dan6200 <danielnyong66@gmail.com>
2021-03-09 21:42:19 -0500

    PRNG

---
+++ LFSR.s
@@ -0,0 +1,33 @@
+.global main
+
+.text
+main:
+	xor %rax,%rax
+	movq $0xa83ef90aa31bcde8, %rax
+	movq $100,%rcx
+rep:
+	pushq %rcx
+	call lfsr
+	popq %rcx
+	loop rep
+	ret
+
+lfsr:
+	movq %rax, %rbx
+	movq %rax, %rcx
+	movq %rax, %rdx
+	shl $63, %rbx 
+	shl $31, %rcx 
+	shl $20, %rdx 
+	xor %rdx, %rcx
+	xor %rcx, %rbx
+	shld $1, %rbx, %rax
+	movq $fmt, %rdi
+	movq %rax, %rsi
+	pushq %rax
+	xor %rax,%rax
+	call printf
+	popq %rax
+	ret
+fmt:
+	.asciz "%llu\n"
